53989d09ef24d63e61b87a59e7eda1830c3eddb4
Jan Jirsík <jjirsik@gmail.com>
2022-02-01 09:06:38 +0100

    day17 MW-starter

---
+++ day17/MW/src/js/script.js
@@ -0,0 +1,6 @@
+import { ProgressBar } from './ProgressBar';
+
+document.addEventListener('DOMContentLoaded', () => {
+  // @TODO no.1 create new instance of ProgressBar and append its right one property to body element
+  // @TODO no.2 fix missing icon on plus button
+});
