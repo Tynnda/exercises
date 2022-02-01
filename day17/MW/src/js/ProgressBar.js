bd809741eb50e470c06248e2cd0d047c95708c0b
Jan Jirsík <jjirsik@gmail.com>
2022-02-01 10:08:46 +0100

    day17 MW-solution

--- day17/MW/src/js/ProgressBar.js
+++ day17/MW/src/js/ProgressBar.js
@@ -1,9 +1,8 @@
 export class ProgressBar {
-  constructor() {
-    this.value = 0;
-    this.gradient = 1;
-    //@TODO no.4 make it possible to create progress bar with any default value and gradient. these values should be passed to constructor
-    //@TODO no.5 make it possible to create progress bar with any maximum value. once using maximum value, make sure the width of knob element is calculated correctly
+  constructor(defaultValue, gradient, maximumValue) {
+    this.value = defaultValue;
+    this.gradient = gradient;
+    this.maximumValue = maximumValue;
 
     this.element = document.createElement('div');
     this.refreshElement();
@@ -12,12 +11,12 @@ export class ProgressBar {
   refreshElement() {
     this.element.innerHTML = `
       <div class="label">
-        ${this.value}/10
+        ${this.value}/${this.maximumValue}
       </div>
       <div class="progress">
          <div class="btn-minus"></div>
          <div class="bar">
-           <div style="width: ${this.value * 10}%" class="knob"></div>
+           <div style="width: ${this.value * (100 / this.maximumValue)}%" class="knob"></div>
          </div>
          <div class="btn-plus"></div>
       </div>
@@ -25,10 +24,13 @@ export class ProgressBar {
 
     const plusBtn = this.element.querySelector('.btn-plus');
     plusBtn.addEventListener('click', () => {
-      this.value = Math.min(10, this.value + this.gradient);
+      this.value = Math.min(this.maximumValue, this.value + this.gradient);
       this.refreshElement();
     });
     const minusBtn = this.element.querySelector('.btn-minus');
-    //@TODO no.3 implement minusBtn listener. if you assign new value to property value, make sure, it is not greater then 10 (or any other maximum value)
+    minusBtn.addEventListener('click', () => {
+      this.value = Math.max(0,  this.value - this.gradient);
+      this.refreshElement();
+    });
   }
 }
--- day17/MW/src/js/script.js
+++ day17/MW/src/js/script.js
@@ -1,6 +1,7 @@
 import { ProgressBar } from './ProgressBar';
 
 document.addEventListener('DOMContentLoaded', () => {
-  // @TODO no.1 create new instance of ProgressBar and append its right one property to body element
-  // @TODO no.2 fix missing icon on plus button
+  const progressBar = new ProgressBar(4, 2, 30);
+
+  document.body.appendChild(progressBar.element);
 });
